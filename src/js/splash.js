c90a488044d426f87469253d9ad72c11702b6334
genesisnp <genesis.np28@gmail.com>
2018-08-03 02:19:06 -0500

    modificando rutas html

--- src/js/splash.js
+++ src/js/splash.js
@@ -1,5 +1,12 @@
-setTimeout(() => {
+// setTimeout(() => {
 
-  window.location.assign("/src/html/principal.html");
-}, 2000)
+//   window.location.assign("/src/html/principal.html");
+// }, 2000)
 
+window.onload=timeout;
+function timeout(){
+window.setTimeout("redirect()",2000)}
+
+function redirect(){
+window.location="/src/html/principal.html"
+return}
